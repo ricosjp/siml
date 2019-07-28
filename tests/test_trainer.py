6b1949aaa9e4de5e2013bddbd8b98723d4b731dc
horiem <yellowshippo@gmail.com>
2019-07-29 05:05:14 +0900

    add gpu testing

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,6 +1,7 @@
 import shutil
 import unittest
 
+from chainer import testing
 import numpy as np
 
 import siml.setting as setting
@@ -39,6 +40,17 @@ class TestTrainer(unittest.TestCase):
     def test_train_general_block_input_selection(self):
         main_setting = setting.MainSetting.read_settings_yaml(
             'tests/data/deform/general_block_input_selection.yml')
+        tr = trainer.Trainer(main_setting)
+        if tr.setting.trainer.output_directory.exists():
+            shutil.rmtree(tr.setting.trainer.output_directory)
+        loss = tr.train()
+        np.testing.assert_array_less(loss, 1.)
+
+    @testing.attr.multi_gpu(2)
+    def test_train_general_block_gpu(self):
+        main_setting = setting.MainSetting.read_settings_yaml(
+            'tests/data/deform/general_block.yml')
+        main_setting.trainer.gpu_id = 1
         tr = trainer.Trainer(main_setting)
         if tr.setting.trainer.output_directory.exists():
             shutil.rmtree(tr.setting.trainer.output_directory)
