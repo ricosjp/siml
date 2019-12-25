16c7d55f498be6901aac790a8dd3be2fed928616
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-12-25 23:54:44 +0900

    Fix format

--- siml/__init__.py
+++ siml/__init__.py
@@ -5,4 +5,4 @@ SiML
 import os.path as _op
 from glob import glob as _glob
 
-__all__ = [_op.splitext(module_name)[0] for module_name in  _glob("*.py")]
+__all__ = [_op.splitext(module_name)[0] for module_name in _glob("*.py")]
