9ca658fd3dd2c73acda19a9a11c9609292f494ef
horiem <yellowshippo@gmail.com>
2020-09-09 22:24:02 +0900

    add gpu device send for dict input data

--- tests/gpu/test_networks_gpu.py
+++ tests/gpu/test_networks_gpu.py
@@ -17,6 +17,19 @@ PLOT = False
 class TestNetworksGPU(unittest.TestCase):
 
     @testing.attr.multi_gpu(2)
+    def test_iso_gcn_gpu(self):
+        main_setting = setting.MainSetting.read_settings_yaml(Path(
+            'tests/data/rotation_thermal_stress/'
+            'iso_gcn_dict_input_dict_output.yml'))
+        main_setting.trainer.gpu_id = 1
+        main_setting.trainer.num_workers = 0  # Serial
+        tr = trainer.Trainer(main_setting)
+        if tr.setting.trainer.output_directory.exists():
+            shutil.rmtree(tr.setting.trainer.output_directory)
+        loss = tr.train()
+        np.testing.assert_array_less(loss, 1.)
+
+    @testing.attr.multi_gpu(2)
     def test_nri_gpu(self):
         main_setting = setting.MainSetting.read_settings_yaml(
             Path('tests/data/deform/nri.yml'))
