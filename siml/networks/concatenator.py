88ab856700b3557a84e6c718440cb6e8390431af
horiem <yellowshippo@gmail.com>
2021-07-13 01:19:12 +0900

    restructure networks.Network

--- siml/networks/concatenator.py
+++ siml/networks/concatenator.py
@@ -7,6 +7,37 @@ from . import siml_module
 class Concatenator(siml_module.SimlModule):
     """Concatenation operation block."""
 
+    @staticmethod
+    def get_name():
+        return 'concatenator'
+
+    @staticmethod
+    def accepts_multiple_inputs():
+        return True
+
+    @staticmethod
+    def is_trainable():
+        return False
+
+    @staticmethod
+    def uses_support():
+        return False
+
+    @classmethod
+    def _get_n_input_node(
+            cls, block_setting, predecessors, dict_block_setting,
+            input_length):
+        return sum([
+            dict_block_setting[predecessor].nodes[-1]
+            for predecessor in predecessors])
+        return input_length
+
+    @classmethod
+    def _get_n_output_node(
+            cls, input_node, block_setting, predecessors, dict_block_setting,
+            output_length):
+        return input_node
+
     def __init__(self, block_setting):
         super().__init__(block_setting, no_parameter=True)
         return
--- siml/networks/identity.py
+++ siml/networks/identity.py
@@ -4,6 +4,34 @@ from . import siml_module
 class Identity(siml_module.SimlModule):
     """Identity block."""
 
+    @staticmethod
+    def get_name():
+        return 'identity'
+
+    @staticmethod
+    def is_trainable():
+        return False
+
+    @staticmethod
+    def accepts_multiple_inputs():
+        return False
+
+    @staticmethod
+    def uses_support():
+        return False
+
+    @classmethod
+    def _get_n_input_node(
+            cls, block_setting, predecessors, dict_block_setting,
+            input_length):
+        return input_length
+
+    @classmethod
+    def _get_n_output_node(
+            cls, input_node, block_setting, predecessors, dict_block_setting,
+            output_length):
+        return input_node
+
     def __init__(self, block_setting):
         super().__init__(block_setting, no_parameter=True)
         return
