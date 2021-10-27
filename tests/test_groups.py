025bbf4d41bdc8861497ccb16c9b46a878296af9
horiem <yellowshippo@gmail.com>
2021-10-28 06:44:42 +0900

    update test

--- tests/test_groups.py
+++ tests/test_groups.py
@@ -30,13 +30,14 @@ class TestGroups(unittest.TestCase):
         np.testing.assert_array_less(loss, 1.)
 
     def test_heat_group_repeat(self):
+        # NL solver repeat
         main_setting = setting.MainSetting.read_settings_yaml(
-            Path('tests/data/heat_time_series/heat_group_repeat.yml'))
+            Path('tests/data/heat_time_series/heat_group_nl_repeat.yml'))
         tr = trainer.Trainer(main_setting)
         if tr.setting.trainer.output_directory.exists():
             shutil.rmtree(tr.setting.trainer.output_directory)
-        loss = tr.train()
-        np.testing.assert_array_less(loss, 1.)
+        loss_implicit = tr.train()
+        np.testing.assert_array_less(loss_implicit, 1.)
 
         ir = inferer.Inferer(
             main_setting,
@@ -47,6 +48,25 @@ class TestGroups(unittest.TestCase):
             output_directory_base=tr.setting.trainer.output_directory,
             data_directories=main_setting.data.preprocessed_root)
 
+        # Simple repeat
+        main_setting = setting.MainSetting.read_settings_yaml(
+            Path('tests/data/heat_time_series/heat_group_repeat.yml'))
+        tr = trainer.Trainer(main_setting)
+        if tr.setting.trainer.output_directory.exists():
+            shutil.rmtree(tr.setting.trainer.output_directory)
+        loss_repeat = tr.train()
+        np.testing.assert_array_less(loss_repeat, 1.)
+
+        ir = inferer.Inferer(
+            main_setting,
+            converter_parameters_pkl=main_setting.data.preprocessed_root
+            / 'preprocessors.pkl')
+        ir.infer(
+            model=main_setting.trainer.output_directory,
+            output_directory_base=tr.setting.trainer.output_directory,
+            data_directories=main_setting.data.preprocessed_root)
+
+        # No repeat
         main_setting_wo_repeat = setting.MainSetting.read_settings_yaml(
             Path('tests/data/heat_time_series/heat.yml'))
         tr_wo_repeat = trainer.Trainer(main_setting_wo_repeat)
@@ -65,3 +85,6 @@ class TestGroups(unittest.TestCase):
             output_directory_base=tr_wo_repeat.setting
             .trainer.output_directory,
             data_directories=main_setting_wo_repeat.data.preprocessed_root)
+
+        self.assertLess(loss_implicit, loss_repeat)
+        self.assertLess(loss_repeat, loss_wo_repeat)
