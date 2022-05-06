e50cba31e0e2d4ab6fd9678481cbfdf16346cd27
horiem <yellowshippo@gmail.com>
2022-05-06 23:43:22 +0900

    update plot_losses

--- siml/__main__/plot_losses.py
+++ siml/__main__/plot_losses.py
@@ -1,6 +1,7 @@
 import argparse
-import pathlib
 from distutils.util import strtobool
+import pathlib
+import re
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -69,6 +70,16 @@ def main():
         nargs='+',
         default=None,
         help='Y limit of the plot')
+    parser.add_argument(
+        '-a', '--x-axis',
+        type=str,
+        default='epoch',
+        help='X axis name')
+    parser.add_argument(
+        '-c', '--continuous',
+        type=strtobool,
+        default=0,
+        help='If True, take into account assume continous training [True]')
     args = parser.parse_args()
 
     csv_files = siml.util.collect_files(
@@ -95,22 +106,50 @@ def main():
     plt.figure(figsize=(16, 9))
     styles = ['-', '--', '-.']
 
+    label_flag = []
     for i, sorted_index in enumerate(sorted_indices):
         df = data_frames[sorted_index]
         name = valid_csv_files[sorted_index]
-        alpha = .5 + 0.5 * (n_seleced_files - 1 - i) / (n_seleced_files - 1)
-        lw = 1. + 2. * (n_seleced_files - 1 - i) / (n_seleced_files - 1)
-        plt.plot(
-            df['epoch'], df['validation_loss'], styles[i % len(styles)],
-            color=cmap(norm(n_seleced_files - 1 - i)),
-            alpha=alpha, lw=lw, label=name)
+        if n_seleced_files == 1:
+            alpha = 1.
+            lw = 1.
+        else:
+            alpha = .5 + 0.5 * (
+                n_seleced_files - 1 - i) / (n_seleced_files - 1)
+            lw = 1. + 2. * (n_seleced_files - 1 - i) / (n_seleced_files - 1)
+        if args.continuous:
+            x_values, index = cumulate_x_values(
+                name, df, valid_csv_files, data_frames, sorted_indices,
+                x_axis=args.x_axis)
+            color = cmap(norm(n_seleced_files - 1 - index))
+        else:
+            x_values = df[args.x_axis]
+            color = cmap(norm(n_seleced_files - 1 - i))
+
+        match = re.search(r'cont(\d+)', name)
+        if match is None:
+            focus_continue_step = 0
+            root_string = str(pathlib.Path(name).parent)
+        else:
+            focus_continue_step = int(match.groups()[0])
+            root_string = name.split(f"_cont{focus_continue_step}")[0]
+        if root_string not in label_flag:
+            plt.plot(
+                x_values, df['validation_loss'], styles[i % len(styles)],
+                color=color, alpha=alpha, lw=lw, label=name)
+            label_flag.append(root_string)
+        else:
+            # Plot without label
+            plt.plot(
+                x_values, df['validation_loss'], styles[i % len(styles)],
+                color=color, alpha=alpha, lw=lw)
+
         validation_min_index = np.argmin(df['validation_loss'].values)
         if args.plot_minimum_loss:
             plt.plot(
-                df['epoch'][validation_min_index],
+                x_values[validation_min_index],
                 df['validation_loss'][validation_min_index],
-                '*', ms=lw*5, color=cmap(norm(n_seleced_files - 1 - i)),
-                alpha=alpha)
+                '*', ms=lw*5, color=color, alpha=alpha)
         print(f"--\nFile name: {name}")
         print(
             f"\t     Best epoch: {df['epoch'][validation_min_index]}\n"
@@ -119,12 +158,11 @@ def main():
             f"\t     Train loss: {df['train_loss'][validation_min_index]}")
         if args.plot_train_loss:
             plt.plot(
-                df['epoch'], df['train_loss'], ':',
-                color=cmap(norm(n_seleced_files - 1 - i)),
-                alpha=alpha, lw=lw)
+                x_values, df['train_loss'], ':',
+                color=color, alpha=alpha, lw=lw)
 
     plt.yscale('log')
-    plt.xlabel('epoch')
+    plt.xlabel(args.x_axis)
     plt.ylabel('loss')
     plt.xlim(0, None)
     if args.x_limit is not None:
@@ -148,6 +186,48 @@ def main():
         print(f"Figure saved in: {file_name}")
 
     return
+
+
+def cumulate_x_values(
+        focus_file_name, focus_data_frame, file_names, data_frames,
+        sorted_indices, x_axis):
+
+    candidate_indices = [
+        i for i, sorted_index in enumerate(sorted_indices)
+        if focus_file_name == file_names[sorted_index]]
+    if len(candidate_indices) != 1:
+        raise ValueError(
+            f"{len(candidate_indices)} files found for: {focus_file_name}")
+    index = candidate_indices[0]
+
+    match = re.search(r'cont(\d+)', focus_file_name)
+    if match is None:
+        focus_continue_step = 0
+        root_string = str(pathlib.Path(focus_file_name).parent)
+    else:
+        focus_continue_step = int(match.groups()[0])
+        root_string = focus_file_name.split(f"_cont{focus_continue_step}")[0]
+
+    x_offset = 0
+    for data_frame, file_name in zip(data_frames, file_names):
+        if root_string not in file_name:
+            continue
+        match = re.search(r'cont(\d+)', file_name)
+        if match is None:
+            continue_step = 0
+        else:
+            continue_step = int(match.groups()[0])
+
+        if continue_step < focus_continue_step:
+            x_offset += np.max(data_frame[x_axis].values)
+
+        candidate_indices = [
+            i for i, sorted_index in enumerate(sorted_indices)
+            if file_name == file_names[sorted_index]]
+        if len(candidate_indices) == 1:
+            index = min(index, candidate_indices[0])
+
+    return x_offset + focus_data_frame[x_axis].values, index
 
 
 def load_logs(csv_files):
