9e78c5eb726738f4b8fd86e275fd3aae80e921b1
horiem <yellowshippo@gmail.com>
2022-08-19 01:47:35 +0900

    update for ci

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -492,11 +492,11 @@ class TestTrainer(unittest.TestCase):
                 / 'train/tet2_3_modulusx0.9000/elemental_strain.npy.enc',
                 allow_pickle=True)
 
-        with self.assertRaises((pickle.UnpicklingError, OSError)):
+        with self.assertRaises(ValueError):
             np.load(
                 main_setting.data.preprocessed[0]
                 / 'train/tet2_3_modulusx0.9000/elemental_strain.npy.enc')
-        with self.assertRaises(OSError):
+        with self.assertRaises((pickle.UnpicklingError, OSError)):
             np.load(
                 main_setting.data.preprocessed[0]
                 / 'train/tet2_3_modulusx0.9000/elemental_strain.npy.enc',
