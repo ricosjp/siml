555451f22c282ca8ef62598a64e6b0a71164ce6d
sakamoto <sakamoto@ricos.co.jp>
2022-09-13 17:16:50 +0900

    [add] user defined preprocess scalar and Threshold Layer

--- siml/networks/__init__.py
+++ siml/networks/__init__.py
@@ -33,6 +33,7 @@ from . import tensor_operations
 from . import time_norm
 from . import translator
 from . import projection
+from . import threshold
 
 
 # Add block information
@@ -62,6 +63,7 @@ blocks = [
     time_norm.TimeNorm,
     translator.Translator,
     projection.Projection,
+    threshold.Threshold,
 
     # Layers with weights
     deepsets.DeepSets,
