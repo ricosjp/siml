937f3635598f180922988078802a7b7f55f9097f
horiem <yellowshippo@gmail.com>
2022-10-14 15:14:34 +0900

    update test data

--- tests/test_dggnn.py
+++ tests/test_dggnn.py
@@ -46,9 +46,9 @@ class TestNetworks(unittest.TestCase):
         print(prediction_conservation - target_conservation)
         return
 
-    def test_dggnn(self):
+    def test_simplest_dggnn(self):
         main_setting = setting.MainSetting.read_settings_yaml(
-            Path('tests/data/advection/dggnn.yml'))
+            Path('tests/data/advection/simplest_dggnn.yml'))
         main_setting.trainer.gpu_id = GPU_ID
         tr = trainer.Trainer(main_setting)
         if tr.setting.trainer.output_directory.exists():
