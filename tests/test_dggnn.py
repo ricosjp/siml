d436ca54fdd244e5b1ddb6b5b65787b1f9e66a76
horiem <yellowshippo@gmail.com>
2022-10-14 15:59:54 +0900

    update test

--- tests/test_dggnn.py
+++ tests/test_dggnn.py
@@ -96,7 +96,7 @@ class TestNetworks(unittest.TestCase):
 
     def test_nonlinear_dggnn(self):
         main_setting = setting.MainSetting.read_settings_yaml(
-            Path('tests/data/advection/linear_dggnn.yml'))
+            Path('tests/data/advection/nonlinear_dggnn.yml'))
         main_setting.trainer.gpu_id = GPU_ID
         tr = trainer.Trainer(main_setting)
         if tr.setting.trainer.output_directory.exists():
